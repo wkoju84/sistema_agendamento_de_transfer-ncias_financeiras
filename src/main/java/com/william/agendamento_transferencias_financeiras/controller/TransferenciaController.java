3c699aeea26703c3e7af7344914abe52fc49ab33
wkoju84 <koju_william@hotmail.com>
2026-06-26 13:33:50 -0300

    feat(transferencia): add POST endpoint to schedule transfer

---
+++ src/main/java/com/william/agendamento_transferencias_financeiras/controller/TransferenciaController.java
@@ -0,0 +1,28 @@
+package com.william.agendamento_transferencias_financeiras.controller;
+
+import com.william.agendamento_transferencias_financeiras.dtos.TransferenciaRequest;
+import com.william.agendamento_transferencias_financeiras.dtos.TransferenciaResponse;
+import com.william.agendamento_transferencias_financeiras.service.TransferenciaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/transferencias")
+public class TransferenciaController {
+
+    private final TransferenciaService transferenciaService;
+
+    public TransferenciaController(TransferenciaService transferenciaService) {
+        this.transferenciaService = transferenciaService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TransferenciaResponse agendar(@RequestBody @Valid TransferenciaRequest request){
+        return transferenciaService.agendarTransferencia(request);
+    }
+
+}
