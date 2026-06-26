85662616b09509f95868ecbd6d3b1cfdbad5caff
wkoju84 <koju_william@hotmail.com>
2026-06-25 21:16:06 -0300

    chore: Initialize Spring Boot project with Spring Web, Spring Data JPA, and Database H2

---
+++ src/main/java/com/william/agendamento_transferencias_financeiras/AgendamentoTransferenciasFinanceirasApplication.java
@@ -0,0 +1,13 @@
+package com.william.agendamento_transferencias_financeiras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoTransferenciasFinanceirasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoTransferenciasFinanceirasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/william/agendamento_transferencias_financeiras/AgendamentoTransferenciasFinanceirasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.agendamento_transferencias_financeiras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoTransferenciasFinanceirasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
